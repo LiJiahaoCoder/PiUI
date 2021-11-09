45a0f71f9d6ae00ae61d11b98fadf53b5230e407
Jiahao Li <lijiahao_shandong@163.com>
2021-11-09 08:54:40 +0800

    build: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+const { ESLintOptions } = require('eslint');
+
+/**
+ * @const
+ * @type {ESLintOptions}
+ */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'xo',
+  ],
+  ignorePatterns: [
+    'lib',
+    '__tests__/setup.ts',
+    '*.js',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+  ],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+    'react-hooks/exhaustive-deps': 'error',
+    'react/no-unused-prop-types': 'off',
+  },
+};
+
+module.exports = config;
