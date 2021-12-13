5e26a95a7af24a551497073c8c4e18c182fbfc3f
Jiahao Li <lijiahao_shandong@163.com>
2021-12-13 14:43:47 +0800

    style: characters

--- tests/packages/button/index.test.tsx
+++ tests/packages/button/index.test.tsx
@@ -5,18 +5,16 @@ describe('Button test', () => {
   test('Expect Button include PiUI text', () => {
     const { queryByText } = render(<Button>PiUI</Button>);
 
-    expect(
-      queryByText('PiUI'),
-    ).toBeInTheDocument();
+    expect(queryByText('PiUI')).toBeInTheDocument();
   });
 
-  test('Expect disabled to be true given loading is true', () => {
+  test('Expect disabled to be true Given loading is true', () => {
     const { queryByText } = render(<Button loading>PiUI</Button>);
 
     expect(queryByText('PiUI')).toBeDisabled();
   });
 
-  test('Expect Button should be in document given a Button component with test-aria-label', () => {
+  test('Expect Button should be in document Given a Button component with test-aria-label', () => {
     const ariaLabel = 'test-aria-label';
     const { queryByLabelText } = render(<Button ariaLabel={ariaLabel}>PiUI</Button>);
 
